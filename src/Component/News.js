45a80cf5d82a0961831344690500e53afe031a1a
bhavyPatel13 <frontentdeveloper853@gmail.com>
2025-03-25 18:50:15 +0530

    understand logical and component

--- src/Component/News.js
+++ src/Component/News.js
@@ -1,28 +1,31 @@
 import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 import SPINER from "./SPINER";
+import images from "./assets";
 
 export default class News extends Component {
   constructor() {
     super();
     // console.log("Hello I am a constructor from news component");
-    this.state = {  
+    this.state = {
       articles: [],
       loding : true,
       page : 1
     };
   }
+  
   async componentDidMount(){ 
     // console.log("bhavy");
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=68476c07dc894b00a95fcc4af3fc5a8e&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parseData = await data.json();
-    console.log(parseData);
+    console.log("parseData----------->",JSON.stringify(parseData))
     this.setState({
       articles : parseData.articles, 
       totalResults : parseData.totalResults,
       loding : false
     });
+    console.log("pageSize ----------->",this.props.pageSize);
   }
 
   handlerPreviousClick = async () => {
@@ -31,7 +34,7 @@ export default class News extends Component {
     this.setState({loding : true})
     let data = await fetch(url);
     let parseData = await data.json();
-    console.log(parseData);
+    console.log("previous parseData----------->",JSON.stringify(parseData));
     this.setState({
       page : this.state.page - 1,
       articles: parseData.articles,
@@ -68,7 +71,7 @@ export default class News extends Component {
                 <Newsitem
                   title={element.title ? element.title.slice(0,45) : "Bhavy"}
                   description={element.description ? element.description.slice(0,85) : "Kunj"}
-                  imageUrl={!element.urlToImage ? "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" : element.urlToImage}
+                  imageUrl={!element.urlToImage ? images.dummy : element.urlToImage}
                   newsUrl={element.url}/>
               </div>
             })}
---
+++ src/Component/assets/index.js
@@ -0,0 +1,4 @@
+const images = {
+    dummy:'https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg'
+}
+export default images;
